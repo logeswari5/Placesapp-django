f55b9d26aace6a17f3084af5ea9f8ef79cbb1acb
HP <logeswari5>
2021-04-12 19:20:58 +0530

    Add url links for sortcity

--- places/urls.py
+++ places/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
 
     path('places/', views.PlaceListView.as_view(), name="places"),
     path('place/<int:pk>/', views.PlaceDetailView.as_view(), name="place"),
-       ]
+    path('sortcity/', views.sortcity, name="sort_by_city"),
+
+    path('distinct_cities/<str:cityname>', views.all_place_in_city, name="instance-city-place"),
+]
