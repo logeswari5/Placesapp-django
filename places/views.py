b4aa0ca2fe16ce3f53e03a4daf3e702a7f04d7fe
HP <logeswari5>
2021-04-12 19:17:46 +0530

    Add sort_by_city url for filter cities

--- places/views.py
+++ places/views.py
@@ -3,27 +3,30 @@ from django.shortcuts import render
 from django.views import generic
 
 from places.models import Place
+from django.http import HttpResponseRedirect
 
 
 def index(request):
     return render(request, 'places/index.html')
 
 
+
+
 class PlaceDetailView(generic.DetailView):
     model = Place
     template_name = 'places/place.html'
     context_object_name = 'place'
-    paginate_by = 3
+    paginate_by = 2
     queryset = Place.objects.filter(location__isnull=False)
+
 
 
 class PlaceListView(generic.ListView):
     model = Place
     template_name = 'places/places.html'
     context_object_name = 'places'
-    paginate_by = 3
+    paginate_by = 2
     queryset = Place.objects.filter(location__isnull=False)
-
 
 class PlaceCreate(generic.CreateView):
     model = Place
